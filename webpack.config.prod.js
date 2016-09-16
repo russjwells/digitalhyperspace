664baa84d150473d27778663eb31deab9f73afcb
Russ Wells <russ@russdigital.com>
2016-09-15 17:07:06 -0700

    heroku prep noverify

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -4,17 +4,17 @@ var ExtractTextPlugin = require('extract-text-webpack-plugin');
 module.exports = {
   devtool: 'source-map',
 
-  entry: __dirname + "/client/index.js",
+  entry: ['babel-polyfill', __dirname + "/client/index.js"] ,
 
   output: {
     path: __dirname + '/static/dist/',
     filename: 'bundle.js',
   },
-  
+
   resolve: {
     extensions: ['', '.js', '.jsx'],
   },
-  
+
   module: {
     loaders: [
       {
