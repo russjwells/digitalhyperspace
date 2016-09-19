732ceb694944679b44b38d18d34ab6da76c8973b
Russ Wells <russ@russdigital.com>
2016-09-18 19:25:32 -0700

    some changes

--- shared/components/Footer/Footer.jsx
+++ shared/components/Footer/Footer.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 
 function Footer() {
+
+
   return (
    <div className="footer">
      <p>&copy; 2016 &middot; Digital Hyperspace &middot; Russell Wells</p>
-     <p>We are on Twitter : <a href="https://twitter.com/@digitalhyper_" target="_Blank">@digitalhyper_</a></p>
+     <p>We are on Twitter : <a href="https://twitter.com/@digitalhyper_" target="_Blank">@digitalhyper_</a> and Facebook : <a href="https://facebook.com/digitalhyperspace" target="_Blank">@digitalhyperspace</a></p>
    </div>
  );
 }
