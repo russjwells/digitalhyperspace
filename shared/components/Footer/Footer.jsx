537734a89b64d7901ed41f564a366892c049ad99
Russ Wells <russ@russdigital.com>
2016-09-19 20:15:49 -0700

    footer updates

--- shared/components/Footer/Footer.jsx
+++ shared/components/Footer/Footer.jsx
@@ -5,8 +5,9 @@ function Footer() {
 
   return (
    <div className="footer">
-     <p>&copy; 2016 &middot; Digital Hyperspace &middot; Russell Wells</p>
+     <p>&copy; 2016 &middot; Digital Hyperspace &middot;</p>
      <p>We are on Twitter : <a href="https://twitter.com/@digitalhyper_" target="_Blank">@digitalhyper_</a> and Facebook : <a href="https://facebook.com/digitalhyperspace" target="_Blank">@digitalhyperspace</a></p>
+     <p>Made with Love on Earth</p>
    </div>
  );
 }
