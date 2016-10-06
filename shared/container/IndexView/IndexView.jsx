15f085ef7347687a512a95ee35718c91cbdc3d0e
Russ Wells <russ@russdigital.com>
2016-10-05 18:07:42 -0700

    project view updates

--- shared/container/IndexView/IndexView.jsx
+++ shared/container/IndexView/IndexView.jsx
@@ -13,12 +13,18 @@ class IndexView extends Component {
 				<IntroCarosel />
 	        </div>
 	        <div className="screen" id="services">
-				<h3>Experimental Technology</h3>
-				<p>Virtual and augmented reality technologies, evolutionary gaming modalities, immersive networked social spaces...</p>
-				<h3>Websites // Web Apps</h3>
-				<p>We design and engineer state of the art pieces of internet. Need an online store? Portfolio or artist’s page? Blog? Something Bigger? We’d love to make your vision manifest.</p>
-				<h3>Mobile</h3>
-				<p>Native cross-platform mobile development</p>
+	        	<div className="serviceDesc">
+					<h3>Experimental Technology</h3>
+					<p>Virtual and augmented reality technologies, evolutionary gaming modalities, immersive networked social spaces...</p>
+				</div>
+				<div className="serviceDesc">
+					<h3>Websites // Web Apps</h3>
+					<p>We design and engineer state of the art pieces of internet. Need an online store? Portfolio or artist’s page? Blog? Something Bigger? We’d love to make your vision manifest.</p>
+				</div>
+				<div className="serviceDesc">
+					<h3>Mobile</h3>
+					<p>Native cross-platform mobile development</p>
+	        	</div>
 	        </div>
 	        <div className="screen" id="contact">
 				contact form
--- shared/container/ProjectsView/ProjectsView.jsx
+++ shared/container/ProjectsView/ProjectsView.jsx
@@ -4,7 +4,24 @@ function ProjectsView(props, context){
 	return (
 		<div className="content">
 			<div className="screen">
-				Projects
+				<h3>Sitting</h3>
+				<p>a distraction free meditation timer.</p>
+			</div>
+			<div className="screen">
+				<h3>Reddit Clone</h3>
+				<p>an angular demo.</p>
+			</div>
+			<div className="screen">
+				<h3>Github Notetaker</h3>
+				<p>a react demo.</p>
+			</div>
+			<div className="screen">
+				<h3>Harmonize</h3>
+				<p>an educational permaculture game demo.</p>
+			</div>
+			<div className="screen">
+				<h3>Unity3D Networked FPS Roguelike</h3>
+				<p>a mystical multiplayer adventure</p>
 			</div>
 		</div>
 	);
